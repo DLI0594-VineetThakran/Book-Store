fa215c5526b5398cf8546a31576c529b499ce920
Debajyoti Datta <Debajyoti.Datta@godigit.com>
2024-08-27 15:35:31 +0530

    added: cartdto

---
+++ bookStore/src/main/java/com/book/store/dto/cartdto/CartItemDTO.java
@@ -0,0 +1,15 @@
+package com.book.store.dto.cartdto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItemDTO {
+    private Long id;
+    private Long productId;
+    private Integer quantity;
+}
+
