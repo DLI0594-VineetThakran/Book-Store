c5eb073ba0ffb6efb6b125e23883545451787464
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-27 17:08:29 +0530

    Modified : AdminJwtUtil.java

--- bookStore/src/main/java/com/book/store/jwtutil/adminjwtutil/AdminJwtUtil.java
+++ bookStore/src/main/java/com/book/store/jwtutil/adminjwtutil/AdminJwtUtil.java
@@ -1,2 +1,44 @@
-package com.book.store.jwtutil.adminjwtutil;public class AdminJwtUtil {
+package com.book.store.jwtutil.adminjwtutil;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class AdminJwtUtil {
+    private final String SECRET_KEY = "1234";
+
+    public String generateToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()  + 1000 * 60 * 60 *10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public Claims extractClaims(String token){
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    public String extractUsername(String token){
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public boolean isTokenExpired(String token){
+        return extractClaims(token).getExpiration().before(new Date());
+    }
 }
