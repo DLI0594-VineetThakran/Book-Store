4e98ab52026d0e19026f72a90cdf61b6394d6d69
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-27 17:35:23 +0530

    Modified : AdminService.

--- bookStore/src/main/java/com/book/store/service/adminservice/AdminService.java
+++ bookStore/src/main/java/com/book/store/service/adminservice/AdminService.java
@@ -12,11 +12,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Service
-public class AdminService implements AdminServiceImplementation{
+public class AdminService implements AdminServiceInterface {
     @Autowired
     private AdminRepository adminRepository;
 
