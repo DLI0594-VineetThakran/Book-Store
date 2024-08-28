4d6601a9f8335bc00020955d8b842dbfb82acb40
Unnimaya T <Unnimaya.T@godigit.com>
2024-08-28 10:32:16 +0530

    Modified : User and Feedback.

--- bookStore/src/main/java/com/book/store/repository/feedbackrepository/FeedBackRepository.java
+++ bookStore/src/main/java/com/book/store/repository/feedbackrepository/FeedBackRepository.java
@@ -1,9 +1,12 @@
-package com.book.store.repository.feedback;
+package com.book.store.repository.feedbackrepository;
 
 import com.book.store.model.feedbackmodel.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FeedBackRepository extends JpaRepository<Feedback,Long> {
+    List<Feedback> findByProductId(Long productId);
 }
