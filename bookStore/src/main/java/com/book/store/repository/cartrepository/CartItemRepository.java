d9e0203b2fc39b1ca88841d2203c49876a849c0a
Debajyoti Datta <Debajyoti.Datta@godigit.com>
2024-08-29 10:24:11 +0530

    changes

--- bookStore/src/main/java/com/book/store/repository/cartrepository/CartItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.book.store.repository.cartrepository;
-
-import com.book.store.model.cartmodel.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
