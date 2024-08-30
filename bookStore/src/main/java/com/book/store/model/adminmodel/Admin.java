b1e9374c8968b160f1511aa5b528b51c647e3cde
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-30 09:48:20 +0530

    Validation added for User and Admin Controller.

--- bookStore/src/main/java/com/book/store/model/adminmodel/Admin.java
+++ bookStore/src/main/java/com/book/store/model/adminmodel/Admin.java
@@ -1,6 +1,10 @@
 package com.book.store.model.adminmodel;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,12 +22,18 @@ public class Admin {
     private Long id;
 
     @Column(unique = true, nullable = false)
+    @NotBlank(message = "Username is mandatory")
+    @Pattern(regexp = "^[a-zA-Z0-9]{3,20}$", message = "Username must be between 3 and 20 characters and contain only letters and numbers")
     private String username;
 
     @Column(unique = true, nullable = false)
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
     private String email;
 
     @Column(nullable = false)
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
 
     @Column(nullable = false)
--- bookStore/src/main/java/com/book/store/model/usermodel/User.java
+++ bookStore/src/main/java/com/book/store/model/usermodel/User.java
@@ -4,6 +4,10 @@ import com.book.store.model.cartmodel.Cart;
 import com.book.store.model.wishlistmodel.Wishlist;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -24,12 +28,18 @@ public class User {
     private Long id;
 
     @Column(unique = true, nullable = false)
+    @NotBlank(message = "Username is mandatory")
+    @Pattern(regexp = "^[a-zA-Z0-9]{3,20}$", message = "Username must be between 3 and 20 characters and contain only letters and numbers")
     private String username;
 
     @Column(unique = true, nullable = false)
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
     private String email;
 
     @Column(nullable = false)
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
 
     private LocalDateTime createdAt;
